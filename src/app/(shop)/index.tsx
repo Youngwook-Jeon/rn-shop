6c9b3d1354881ab75a9b37ae19d9ad98c03999fb
Youngwook-Jeon <ela87@naver.com>
2024-10-25 21:43:51 +0900

    Fetch placeholder products / Show them on the home screen

--- src/app/(shop)/index.tsx
+++ src/app/(shop)/index.tsx
@@ -1,11 +1,31 @@
-import { StyleSheet, Text, View } from "react-native";
+import { FlatList, StyleSheet, Text, View } from "react-native";
+
+import { PRODUCTS } from "../../../assets/products";
+import { ProductListItem } from "../../components/product-list-item";
+import { ListHeader } from "../../components/list-header";
 
 export default function Home() {
   return (
     <View>
-      <Text>Home</Text>
+      <FlatList
+        data={PRODUCTS}
+        renderItem={({ item }) => <ProductListItem product={item} />}
+        keyExtractor={(item) => item.id.toString()}
+        numColumns={2}
+        ListHeaderComponent={ListHeader}
+        contentContainerStyle={styles.flatListContent}
+        columnWrapperStyle={styles.flatListColumn}
+        style={{ paddingHorizontal: 10, paddingVertical: 5 }}
+      />
     </View>
   );
 }
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  flatListContent: {
+    paddingBottom: 20,
+  },
+  flatListColumn: {
+    justifyContent: "space-between",
+  },
+});
