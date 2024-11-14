937932ad4f94817e5eb644f218c5832dc3459b7f
Youngwook-Jeon <ela87@naver.com>
2024-11-14 23:21:31 +0900

    Log in by using the supabase's auth utilities

--- src/components/list-header.tsx
+++ src/components/list-header.tsx
@@ -12,9 +12,14 @@ import { FontAwesome } from "@expo/vector-icons";
 
 import { CATEGORIES } from "../../assets/categories";
 import { useCartStore } from "../store/cart-store";
+import { supabase } from "../lib/supabase";
 
 export const ListHeader = () => {
   const { getItemCount } = useCartStore();
+
+  const handleSignOut = async () => {
+    await supabase.auth.signOut();
+  };
 
   return (
     <View style={[styles.headerContainer]}>
@@ -47,7 +52,10 @@ export const ListHeader = () => {
               )}
             </Pressable>
           </Link>
-          <TouchableOpacity style={styles.signOutButton}>
+          <TouchableOpacity
+            onPress={handleSignOut}
+            style={styles.signOutButton}
+          >
             <FontAwesome name="sign-out" size={25} color="red" />
           </TouchableOpacity>
         </View>
